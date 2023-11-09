ddd805a86a2eb8273b62c281738d2dc10803d621
Sean Geelan <seanpatrick.geelan@gmail.com>
2023-11-09 15:36:42 +0100

    mappingicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
       title: "NavVis IVION",
       logo: {
         alt: "NavVis VLX Documentation",
-        src: "img/DeskTop1.png",
+        src: "img/Navvis_Mapping.png",
       },
      
     },
