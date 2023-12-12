e304e5d44de8495f14a2e9ea32db02ea5967975e
Sean Geelan <seanpatrick.geelan@gmail.com>
2023-12-12 19:02:56 +0100

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,15 +5,15 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.svg",
+  favicon: "img/DESKTOP1.png",
   organizationName: "NavVis",
   projectName: "navvis-docusaurus-template",
   themeConfig: {
     navbar: {
       title: "NavVis IVION",
       logo: {
-        alt: "NavVis VLX Documentation",
-        src: "img/Navvis_Mapping.png",
+        alt: "NavVis Documentation",
+        src: "img/DESKTOP1.png",
       },
      
     },
